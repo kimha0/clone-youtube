83b1e387369a66426312e3f2214d6d4264a47b72
kimha0 <hyoreuno@naver.com>
2020-10-06 17:34:23 +0900

    feat: init project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
+import { renderFile } from 'squirrelly';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  app.engine('html', renderFile);
+  app.setViewEngine('squirrelly');
+
   await app.listen(3000);
 }
 bootstrap();
